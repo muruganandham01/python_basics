214b3099ec1324eed656d39ffe274fc18d9653ab
muruganandham01 <suryasm300@gmail.com>
2026-06-20 12:18:53 +0530

    demo

--- variables.py
+++ variables.py
@@ -1,3 +1,26 @@
 print('hello world')
-a='abcd'    #created a string variable
+a='abcd'        #created a string variable
 print(a)
+
+
+b=5             #store the value 5 on variable b
+print(b)
+
+c=5             #c stores the value of intger 5 first
+c="abcd"        #now it update to string abcd
+print(c)
+
+
+
+a=str(5)        #print the integer as string
+b=chr(65)       #print the ascii value of 65 int ---> char
+c=ord('a')      #print the ascii value of a char ---> int
+d=int(10.90)    #print only the integer
+e=float(2)      #print value with decimal point
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+
+
